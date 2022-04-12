2ec46030b3dd7f7ae92f9d3c000e0eb2a2ea9553
GAbriel <gabrielsantos77239@gmail.com>
2022-04-12 16:13:28 -0300

    empresas_id

--- controllers/empresa_controller.js
+++ controllers/empresa_controller.js
@@ -1,6 +1,7 @@
 const mysql = require("../msql").pool;
 
-exports.getPodutos = (req,res,next)=>{
+//retorna todos os produtos da tabela empresas
+exports.getEmpresas = (req,res,next)=> {
     mysql.getConnection((error,conn)=> {
         if(error){return res.status(500).send({ error:error})}
         conn.query(
@@ -11,7 +12,13 @@ exports.getPodutos = (req,res,next)=>{
                     id: result.lenght,
                     empresas: result.map(prod => {
                         return{
-                            empresas: prod.nome,                      
+                            id: prod.id,
+                            empresas: prod.nome, 
+                            request:{
+                                tipo: 'GET',
+                                descricao:'retorna id do produto',
+                                url:'http://localhost:3003/empresas/'+ prod.id
+                            }                     
                         }
                     })
                 }
@@ -20,3 +27,33 @@ exports.getPodutos = (req,res,next)=>{
         )        
     });
 }
+
+//retorna empresa com id
+exports.getEmpresasID = (req,res,next)=> {
+    mysql.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({ error:error})}        
+        conn.query(
+            'SELECT * FROM empresas WHERE id = ?;',[req.params.id],
+            (error,result)=>{
+                if(error){return res.status(500).send({ error:error})}
+                if (result.length == 0) {
+                    return res.status(404).send({
+                        message: 'Não foi encontrado produto com este ID'
+                    })
+                }
+                const response = {
+                    produtoCriado: {
+                        id: result[0].id,
+                        empresas: result[0].empresas,
+                        request:{
+                            tipo: 'GET',
+                            descricao:'retorna um todas empresas',
+                            url:'http://localhost:3003/empresas'
+                        }  
+                    }
+                }
+                return res.status(201).send(response)    
+            }
+        )        
+    }    
+)};
--- route/empresas.js
+++ route/empresas.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const produtosController = require('../controllers/empresa_controller');
+//variavel que recebe o controller empresa
+const empresasController = require('../controllers/empresa_controller');
 
-router.get('/',produtosController.getPodutos);
+// rortas CRUD: GET,GETid,POST,PATCH,DELETE da tabela empresas
 
+//retorna todas as empresas
+router.get('/',empresasController.getEmpresas);
+// retorna empresa com id 
+router.get('/:id',empresasController.getEmpresasID)
 
 
 module.exports = router;
