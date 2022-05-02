639dfe26d3fa1d31d601b5fa8279e9d5cc5d134f
GAbriel <gabrielsantos77239@gmail.com>
2022-05-02 15:43:58 -0300

    api

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 //recebe as rotas
 const routerEmpresas = require('./route/empresas');
