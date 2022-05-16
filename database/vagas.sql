ed27b5a14322e286ae0ccb806c06bc8975e44a86
GAbriel <gabrielsantos77239@gmail.com>
2022-05-16 17:54:04 -0300

    api_finalizada

--- database/vagas.sql
+++ database/vagas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Tempo de geração: 13/05/2022 às 17:22
+-- Tempo de geração: 16/05/2022 às 17:51
 -- Versão do servidor: 8.0.28-0ubuntu0.20.04.3
 -- Versão do PHP: 7.4.3
 
@@ -33,6 +33,16 @@ CREATE TABLE `candidatar` (
   `id_usuario` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Despejando dados para a tabela `candidatar`
+--
+
+INSERT INTO `candidatar` (`id_vaga`, `id_usuario`) VALUES
+(89, 'alan@email.com'),
+(90, 'alan@email.com'),
+(91, 'alan@email.com'),
+(78, 'alan@email.com');
+
 -- --------------------------------------------------------
 
 --
@@ -41,7 +51,7 @@ CREATE TABLE `candidatar` (
 
 CREATE TABLE `empresas` (
   `id` int NOT NULL,
-  `cnpj` varchar(15) DEFAULT NULL,
+  `cnpj` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `nome` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `senha` text NOT NULL,
   `imagem` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
@@ -52,7 +62,8 @@ CREATE TABLE `empresas` (
 --
 
 INSERT INTO `empresas` (`id`, `cnpj`, `nome`, `senha`, `imagem`) VALUES
-(69, '111.222.333', 'teorema', '$2b$10$ryUt5JrniwdGTeFv0cLlIurEsJrBN/72MYxF.SgX6zcj.UYmFRfIG', NULL);
+(70, '123', 'teorema', '$2b$10$siC4x0ohSWzbhcdYUHEBXuVEiBhVu4Lcj9rozAI9g4omHgpLM.CzW', NULL),
+(71, '123456', 'teste', '$2b$10$SjP9bahm8bvgx3UD8hN/ZembHMDFjFuE8eGyKHmBThhx3rqhHOfWO', NULL);
 
 -- --------------------------------------------------------
 
@@ -72,6 +83,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`email`, `senha`, `nome`, `imagem`) VALUES
+('7', '$2b$10$p.1unIJ1G8BUTaC.lxHIIOL5VYrxQ2yqOk2UCUihOQRNanqVPPqRC', '9', NULL),
 ('alan@email.com', '$2b$10$cbOnFDeGTMemSgf5iyPAAueTP7jCVVBzPWeElobt.b77Nya1jjJyG', 'alan', 'uploads/2022-05-11T17:06:23.719ZCaptura de tela de 2022-03-30 14-05-28.png'),
 ('teste2@email', '$2b$10$hJ5vLrjBbkMoo2cbxcRJtuO9vGWJqgzsjSvLFsa/TpHHSnmFtDUzC', 'alan', 'uploads/2022-04-29T16:48:48.448ZCaptura de tela de 2022-04-06 14-26-38.png'),
 ('teste3@email', '$2b$10$D13R0YIYpe7CU.MKGsCQVu0W.PtYiVAfAJC9zspuyNCX61IM/1oQS', 'alan', 'uploads/2022-05-04T18:09:27.944ZCaptura de tela de 2022-03-30 14-05-28.png'),
@@ -85,11 +97,21 @@ INSERT INTO `usuarios` (`email`, `senha`, `nome`, `imagem`) VALUES
 
 CREATE TABLE `vagas_emprego` (
   `id_vag` int NOT NULL,
-  `titulo` varchar(100) DEFAULT NULL,
-  `salario` float DEFAULT NULL,
-  `descricao` text,
+  `titulo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `salario` float NOT NULL,
+  `descricao` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `empresa_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Despejando dados para a tabela `vagas_emprego`
+--
+
+INSERT INTO `vagas_emprego` (`id_vag`, `titulo`, `salario`, `descricao`, `empresa_id`) VALUES
+(78, 'tester', 45, 'ryhry', 71),
+(89, 'tght', 34, 'grg', 70),
+(90, 'rggv', 45354, 'dgd', 70),
+(91, 'rrgg', 3455, 'gg', 70);
 
 --
 -- Índices de tabelas apagadas
@@ -129,13 +151,13 @@ ALTER TABLE `vagas_emprego`
 -- AUTO_INCREMENT de tabela `empresas`
 --
 ALTER TABLE `empresas`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 
 --
 -- AUTO_INCREMENT de tabela `vagas_emprego`
 --
 ALTER TABLE `vagas_emprego`
-  MODIFY `id_vag` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
+  MODIFY `id_vag` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
 
 --
 -- Restrições para dumps de tabelas
